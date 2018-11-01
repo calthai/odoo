9b17010dacd8c6bf8e3a6de298449cf8353107f3
Jatupong <jatupong.a@itaas.co.th>
2018-11-01 07:41:37 +0700

    update

--- capelda_extended/models/product_template.py
+++ capelda_extended/models/product_template.py
@@ -13,96 +13,13 @@ from odoo import api, fields, models, _
 class product_template(models.Model):
     _inherit = 'product.template'
 
-    # is_check_avilable = fields.Boolean(string='Check Avilable', default=True)
-    text_remark = fields.Text(string="Location/QTY", compute='_compute_product_template_availability', copy=False)
+    # # is_check_avilable = fields.Boolean(string='Check Avilable', default=True)
+    # # text_remark = fields.Text(string="Location/QTY",compute='_compute_product_template_availability')
+    # text_remark = fields.Text(string="Location/QTY")
+    stock_info_text = fields.Text(string='Stock Info',compute='_get_product_template_availability')
     sequence_id = fields.Many2one('ir.sequence', string="Sequence ID.", readonly=True, copy=False)
 
-    @api.multi
-    @api.depends('text_remark')
-    def _compute_product_template_availability(self):
-        # print '_compute_product_template_availability'
-        for product in self.env['product.template'].search([]):
-            if product.type == 'product':
-                quant_ids = self.env['stock.quant'].search(
-                    [('product_id', '=', product.id), ('company_id', '=', product.company_id.id)])
-                if quant_ids:
-                    quant_s = {}
-                    i = 0
-                    for quant in quant_ids:
-                        found = False
-                        if i > 0:
-                            # print "more than one"
-                            for x in xrange(0, i):
-                                # first time
-                                # print quant.location_id.id
-                                # print quant_s[x]['location_id']
-                                if quant.location_id.name == quant_s[x]['location_id']:
-                                    found = True
-                                    quant_s[x]['quantity'] += quant.quantity
-                                    break
 
-                            if not found:
-                                # print "not found same product"
-                                quant_s[i] = {
-                                    'location_id': quant.location_id.name,
-                                    'quantity': quant.quantity,
-                                }
-                                i += 1
-                        else:
-                            # print 'first time'
-
-                            quant_s[i] = {
-                                'location_id': quant.location_id.name,
-                                'quantity': quant.quantity,
-                            }
-                            i += 1
-
-                    quant_s = [value for key, value in quant_s.items()]
-                    remark = ''
-                    for qs in quant_s:
-                        remark += str(qs['location_id'])
-                        remark += ' = '
-                        remark += str(qs['quantity'])
-                        remark += ', '
-
-                    product.text_remark = remark
-
-
-    # @api.multi
-    # @api.depends('text_remark')
-    # def _compute_product_template_availability(self):
-    #     # print '_compute_product_template_availability'
-    #     for product in self.env['product.template'].search([]):
-    #         if product.type == 'product':
-    #             quant_ids = self.env['stock.quant'].search([('product_id','=',product.id),('company_id','=',product.company_id.id)])
-    #             print quant_ids
-    #             if quant_ids:
-    #                 location_ids = []
-    #                 remark = ''
-    #                 for quant in quant_ids:
-    #                     if quant.location_id.id not in location_ids:
-    #                         location_ids.append(quant.location_id.id)
-    #                 print location_ids
-    #                 if location_ids:
-    #                     # print '======1'
-    #
-    #                     for quant in quant_ids:
-    #                         if quant.location_id.id in location_ids:
-    #                             # print '===========2'
-    #                             quantity = 0.00
-    #                             for x in range(0, len(location_ids), 1):
-    #                                 # print x
-    #                                 if quant.location_id.id == location_ids[x]:
-    #                                     # print '======3'
-    #                                     # print quant.quantity
-    #                                     quantity += quant.quantity
-    #
-    #                                     remark += quant.location_id.name
-    #                                     remark += ' = '
-    #                                     remark += str(quantity)
-    #                                     remark += ', '
-    #
-    #                 product.text_remark = remark
 
 
     @api.multi
@@ -128,3 +45,55 @@ class product_template(models.Model):
                 # else:
                 #     raise UserError(_('Sequence is button gen.'))
 
+
+    def _get_product_template_availability(self):
+        for product in self:
+            if product.type == 'product':
+                product_id = self.env['product.product'].sudo().search([('product_tmpl_id', '=', product.id)])
+                quant_ids =self.env['stock.quant'].search(
+                                    [('product_id', '=', product_id[0].id), ('company_id', '=', product_id[0].company_id.id)])
+
+                print(quant_ids)
+                if quant_ids:
+                    quant_s = {}
+                    i = 0
+                    for quant in quant_ids:
+                        found = False
+                        if i > 0:
+                            print("more than one")
+                            for x in range(0, i):
+                                # first time
+                                # print quant.location_id.id
+                                # print quant_s[x]['location_id']
+                                if quant.location_id.name == quant_s[x]['location_id']:
+                                    found = True
+                                    quant_s[x]['quantity'] += quant.quantity
+                                    break
+
+                            if not found:
+                                # print "not found same product"
+                                quant_s[i] = {
+                                    'location_id': quant.location_id.name,
+                                    'quantity': quant.quantity,
+                                }
+                                i += 1
+                        else:
+                            print('first time')
+
+                            quant_s[i] = {
+                                'location_id': quant.location_id.name,
+                                'quantity': quant.quantity,
+                            }
+                            i += 1
+
+                    quant_s = [value for key, value in quant_s.items()]
+                    remark = ''
+                    # print(quant_s)
+                    for qs in quant_s:
+                        remark += str(qs['location_id'])
+                        remark += ' = '
+                        remark += str(qs['quantity'])
+                        remark += ', '
+
+                    # print (remark)
+                    product.stock_info_text = remark
